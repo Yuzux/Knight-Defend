b09aba378056dd4b47b4158f7fea01bea99ac17e
Yuzux <61511020+Yuzux@users.noreply.github.com>
2020-03-19 18:24:35 +0100

    Add files via upload

--- jeu.py
+++ jeu.py
@@ -4,6 +4,7 @@ import random
 from os import path
 
 img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 WIDTH = 360
 HEIGHT = 480
@@ -24,15 +25,44 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Knight Defend")
 clock = pygame.time.Clock()
 
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+def newennemy():
+    m = Ennemy()
+    all_sprites.add(m)
+    ennemy.add(m)
+
+def draw_shield_bar(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(player_img, (50, 38))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH /2
+        self.radius = 20
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.shield = 100
+        self.shoot_delay = 300
+        self.last_shot = pygame.time.get_ticks()
     
     def update(self):
         self.speedx = 0
@@ -41,6 +71,8 @@ class Player(pygame.sprite.Sprite):
             self.speedx = -5
         if keystate[pygame.K_RIGHT]:
             self.speedx = 5
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -48,22 +80,41 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
     
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            bullet = Bullet(self.rect.centerx, self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
+            shoot_sound.play()
+            shoot_sound.set_volume(0.1)
 
 class Ennemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.transform.scale(ennemy_img, (60, 48))
-        self.image.set_colorkey(BLACK)
+        self.image_orig = pygame.transform.scale(ennemy_img, (50, 38))
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
+        self.radius = 20 # int(self.rect.width * .9 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            self.image = pygame.transform.rotate(self.image_orig, self.rot_speed)
 
     def update(self):
+        self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
@@ -86,22 +137,67 @@ class Bullet(pygame.sprite.Sprite):
         # kill screen
         if self.rect.bottom < 0:
             self.kill()
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 50
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
+
 # graphic
 background = pygame.image.load(path.join(img_dir, "fond.png")).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_dir, "player.png")).convert()
 ennemy_img = pygame.image.load(path.join(img_dir, "ennemy.png")).convert()
 bullet_img = pygame.image.load(path.join(img_dir, "laser.png")).convert()
+explosion_anim = {}
+explosion_anim['lg'] = []
+explosion_anim['sm'] = []
+for i in range(9):
+    filename = 'regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, filename)).convert()
+    img.set_colorkey(BLACK)
+    img_lg = pygame.transform.scale(img, (75, 75))
+    explosion_anim['lg'].append(img_lg)
+    img_sm = pygame.transform.scale(img, (32, 32))
+    explosion_anim['sm'].append(img_sm)
+
+# sound
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'Laser_Shoot.wav'))
+expl_sounds = []
+for snd in ['Explosion.wav', 'Explosion2.wav']:
+    expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, snd)))
+
+pygame.mixer.music.load(path.join(snd_dir, 'music.wav'))
+pygame.mixer.music.set_volume(0.2)
 
 all_sprites = pygame.sprite.Group()
 ennemy = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-for i in range(5):
-    m = Ennemy()
-    all_sprites.add(m)
-    ennemy.add(m)
+for i in range(8):
+    newennemy()
+score = 0
+pygame.mixer.music.play(-1, 0.0)
 
 if __name__ == "__main__":
     # Game loop
@@ -114,29 +210,37 @@ if __name__ == "__main__":
             # check for closing window
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    player.shoot()
 
         # Update
         all_sprites.update()
 
         # check hit bullet mob
-        hits = pygame.sprite.groupcollide(ennemy, bullets, True, True)
+        hits = pygame.sprite.groupcollide(ennemy, bullets, True, True, pygame.sprite.collide_circle)
         for hit in hits:
-            m = Ennemy()
-            all_sprites.add(m)
-            ennemy.add(m)
+            score += 1
+            random.choice(expl_sounds).play()
+            expl_sounds[0].set_volume(0.1)
+            expl_sounds[1].set_volume(0.1)
+            expl = Explosion(hit.rect.center, 'lg')
+            all_sprites.add(expl)
+            newennemy()
 
         # check colision
-        hits = pygame.sprite.spritecollide(player, ennemy, False)
-        if hits:
-            running = False
+        hits = pygame.sprite.spritecollide(player, ennemy, True, pygame.sprite.collide_circle)
+        for hit in hits:
+            player.shield -= 20
+            expl = Explosion(hit.rect.center, 'sm')
+            all_sprites.add(expl)
+            newennemy()
+            if player.shield <= 0:
+                running = False
 
         # Draw / render
         screen.fill(BLACK)
         screen.blit(background, background_rect)
         all_sprites.draw(screen)
+        draw_text(screen, str(score), 18, WIDTH / 2, 10)
+        draw_shield_bar(screen, 5, 5, player.shield)
         # *after* drawing everything, flip the display
         pygame.display.flip()
 
